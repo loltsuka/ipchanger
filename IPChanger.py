3c2baf7e31021314b309bfbaf841cb3995a8954f
loltsuka <verneri.v@gmail.com>
2018-03-21 23:08:50 +0200

    fixed some path stuff

--- IPChanger.py
+++ IPChanger.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 #IPChanger.py
 
-
 import os
 import subprocess
 import sys
@@ -18,8 +17,8 @@ def create_shortcut():
         with open('shortcutter.desktop', 'w') as f:
             f.write('[Desktop Entry]\n')
             f.write('Name=Test\n')
-            f.write('Exec='+os.getcwd()+'/IPChanger\n')
-            f.write('Icon='+os.getcwd()+'/Thinking.png\n')
+            f.write('Exec='+os.path.abspath(__file__)+'\n')
+            f.write('Icon='+os.path.abspath(__file__)[:-13]+'/Thinking.png\n')
             f.write('Terminal=false\n')
             f.write('Type=Application\n')
             f.write('Name[en_US]=IPChanger\n')
